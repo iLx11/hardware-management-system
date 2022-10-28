d5876723cabad5de02da0b493fd5994d32b7e7c2
iLx1 <c.olor-ful@qq.com>
2022-10-28 12:50:44 +0800

    修改UI

---
+++ src/main/java/com/colorful/config/CorsFilter.java
@@ -0,0 +1,30 @@
+package com.colorful.config;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(filterName = "CorsFilter")
+public class CorsFilter  implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "*");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("字符集编码：");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("销毁");
+    }
+}
